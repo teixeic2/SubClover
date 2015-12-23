d21bfbdaa33def7c427fe565125e96b88fd29823
teixeic2 <teixeic2@icloud.com>
2015-12-23 18:54:20 +1300

    First commit R script :  Percentage of plants with cold damage I2; anova and bar plot graph.

---
+++ ColdPercPlantProject.R
@@ -0,0 +1,39 @@
+library(tidyr)
+library (dplyr)
+library (ggplot2)
+library(agricolae)
+setwd("C:\\Users\\Ed\\Documents\\Subclover\\ColdSens")
+getwd()
+
+#create file
+file <- read.table("ColdPercPlant.txt",header=TRUE)
+file
+head(file)
+summary (file)
+
+#Anova
+file$Block <- as.factor(file$Block )
+anova<-aov(file$PercentageofColddamagedPlants~file$Cultivar+file$Block)
+summary(anova)
+TukeyHSD(anova)
+
+HSD.test(amod, "tx", group=TRUE)
+
+#barplot 
+# The palette with grey:
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#D55E00", "#0072B2")
+
+file %>%
+  #filter(Cultivar == "Antas") %>%
+  group_by(Cultivar) %>%
+  summarise_each(funs(mean, sd)) %>%
+  ggplot(aes(x=Cultivar, y=PercentageofColddamagedPlants_mean, fill=Cultivar)) +
+  geom_bar(position = position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=PercentageofColddamagedPlants_mean-PercentageofColddamagedPlants_sd/2,
+                    ymax=PercentageofColddamagedPlants_mean+PercentageofColddamagedPlants_sd/2),
+                    width=0.25)   +
+  scale_fill_manual(values=cbPalette)
+
+
+
+
