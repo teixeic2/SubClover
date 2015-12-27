a5351808769e272820f4ab0c1ef9e81c152e1308
teixeic2 <teixeic2@icloud.com>
2015-12-27 23:27:12 +1300

    Ordered bars with function mutate and order;  changed x and y labels.

--- ColdPercPlantProject.R
+++ ColdPercPlantProject.R
@@ -14,7 +14,7 @@ summary (file)
 
 #AnovaOut pure numbers
 file$Block <- as.factor(file$Block )
-anovaOut<-aov(file$PercentageofColddamagedPlants~file$Cultivar+file$Block)
+anovaOut<-aov(file$PercDamag~file$Cultivar+file$Block)
 summary(anovaOut)
 TukeyHSD(anovaOut)
 
@@ -22,19 +22,19 @@ TukeyHSD(anovaOut)
 
 #barplot 
 # The palette with grey:
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#D55E00", "#0072B2")
+#cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#D55E00", "#0072B2")
 
 #Here is analysis of Origin
 file %>%
   #filter(Cultivar == "Antas") %>%
   group_by(Origin) %>%
   summarise_each(funs(mean, sd)) %>%
-  ggplot(aes(x=Origin, y=PercentageofColddamagedPlants_mean, fill=Origin)) +
+  ggplot(aes(x=Origin, y=PercDamag_mean, fill=Origin)) +
   geom_bar(position = position_dodge(), stat="identity") +
-  geom_errorbar(aes(ymin=PercentageofColddamagedPlants_mean-PercentageofColddamagedPlants_sd/2,
-                    ymax=PercentageofColddamagedPlants_mean+PercentageofColddamagedPlants_sd/2),
+  geom_errorbar(aes(ymin=PercDamag_mean-PercDamag_sd/2,
+                    ymax=PercDamag_mean+PercDamag_sd/2),
                     width=0.25)   +
-  scale_fill_manual(values=cbPalette) + 
+ # scale_fill_manual(values=cbPalette) + 
   theme_grey(base_size = 16)+
   theme(axis.text.x=element_text(angle = +90, hjust = 0))
 
@@ -45,42 +45,54 @@ file %>%
   #filter(Cultivar == "Antas") %>%
   group_by(Subspecies) %>%
   summarise_each(funs(mean, sd)) %>%
-  ggplot(aes(x=Subspecies, y=PercentageofColddamagedPlants_mean, fill=Subspecies)) +
+  ggplot(aes(x=Subspecies, y=PercDamag_mean, fill=Subspecies)) +
   geom_bar(position = position_dodge(), stat="identity") +
-  geom_errorbar(aes(ymin=PercentageofColddamagedPlants_mean-PercentageofColddamagedPlants_sd/2,
-                    ymax=PercentageofColddamagedPlants_mean+PercentageofColddamagedPlants_sd/2),
+  geom_errorbar(aes(ymin=PercDamag_mean-PercDamag_sd/2,
+                    ymax=PercDamag_mean+PercDamag_sd/2),
                 width=0.25)   +
-  scale_fill_manual(values=cbPalette) + 
+ # scale_fill_manual(values=cbPalette) + 
   theme_grey(base_size = 16)+
   theme(axis.text.x=element_text(angle = +90, hjust = 0))
 
 ggsave(file="PercentagePlants2.tiff", dpi=300)
+
+#structure check
+str()
+
+x <- 1
+x <- c(1,2,3,4,5)
+x <- x*2
+
 
 #Here is analysing Cultivar 
 file %>%
   #filter(Cultivar == "Antas") %>%
   group_by(Cultivar) %>%
   summarise_each(funs(mean, sd)) %>%
-  ggplot(aes(x=Cultivar, y=PercentageofColddamagedPlants_mean, fill=Cultivar)) +
+  mutate(Cultivar = factor(Cultivar,levels = 
+                                Cultivar[order(PercDamag_mean)])) %>%
+  ggplot(aes(x=Cultivar, y=PercDamag_mean, fill=Cultivar)) +
   geom_bar(position = position_dodge(), stat="identity") +
-  geom_errorbar(aes(ymin=PercentageofColddamagedPlants_mean-PercentageofColddamagedPlants_sd/2,
-                    ymax=PercentageofColddamagedPlants_mean+PercentageofColddamagedPlants_sd/2),
+  geom_errorbar(aes(ymin=PercDamag_mean-PercDamag_sd/2,
+                    ymax=PercDamag_mean+PercDamag_sd/2),
                 width=0.25)   +
-  scale_fill_manual(values=cbPalette) + 
+ # scale_fill_manual(values=cbPalette) + 
   theme_grey(base_size = 16)+
-  theme(axis.text.x=element_text(angle = +90, hjust = 0))
+  theme(axis.text.x=element_text(angle = +90, hjust = 0)) +
+  xlab("Cultivar")+
+  ylab("% Damage")
 
 ggsave(file="PercentagePlants3.tiff", dpi=300)
 
 #histogram
-ggplot(data=file, aes(PercentageofColddamagedPlants)) + 
+ggplot(data=file, aes(PercDamag)) + 
   geom_histogram()
 
 #normality test
-shapiro.test(file$PercentageofColddamagedPlants)
+shapiro.test(file$PercDamag)
 
 #QQplot
-var<-file$PercentageofColddamagedPlants
+var<-file$PercDamag
 qqnorm(var)
 qqline(var, col = 2)
 qqplot(var, rt(300, df = 5))
@@ -89,12 +101,12 @@ ggplot(file, aes(sample = var)) + stat_qq()
 
 
 #transform SQR
-file$sQR <- sqrt(file$PercentageofColddamagedPlants)
+file$sQR <- sqrt(file$PercDamag)
 ggplot(data=file, aes(file$sQR)) + 
   geom_histogram()
 
 #transform log
-file$log <- log (file$PercentageofColddamagedPlants)
+file$log <- log (file$PercDamag)
 ggplot(data=file, aes(file$log)) + 
   geom_histogram()
 
@@ -138,7 +150,4 @@ summary (anovaLogOrigin)
 #Means separation of log transformed data
 (HSD.test(anovaLogOrigin, "file$Cultivar"))             
 summary (anovaLogOrigin)
-
-
-
 
