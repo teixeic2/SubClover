a1f14a759798cea0432dc4ea6e27d8d5541e30cc
teixeic2 <teixeic2@icloud.com>
2016-03-10 22:17:40 +1300

    up dated flower

--- SeedSoftProjet.R
+++ SeedSoftProjet.R
@@ -84,11 +84,65 @@ TukeyHSD(anovaSQR)
 (LSD.test(anovaSQR, "file$depth", alpha= 0.05, p.adj="none"))             
 summary (anovaSQR)
 
+# transformations
+
+#data transformation
+file$transf_sqr <- sqrt(file$percentageHard)
+file$transf_log <- log(file$percentageHard)
+file$transf_acos <- acos(file$percentageHard/100)
+file %>%
+  tidyr::gather("transformation","value",10:12) %>%
+  ggplot( aes(value)) + 
+  geom_histogram()+
+  facet_grid(.~transformation, scales="free")
+
+
+
+
+head(file)
+
+#histogram
+ggplot(data=file, aes(transf)) + 
+  geom_histogram()
+
+#normality test
+shapiro.test(file$SQR)
+
+#QQplot
+var<-file$SQR
+qqnorm(var)
+qqline(var, col = 2)
+qqplot(var, rt(300, df = 5))
+
+
+#AnovaLog analysis of transformed sqr
+file$Block <- as.factor(file$Block )
+anovaSQR<-aov(file$SQR ~ file$Cultivar*file$depth+file$Block)
+summary(anovaSQR)
+TukeyHSD(anovaSQR)
+
+#Means separation of SQR transformed data by Cultivar
+(LSD.test(anovaSQR, "file$Cultivar", alpha= 0.05, p.adj="none"))             
+
+#Anova and Means separation of log transformed data by Subspecies
+file$Block <- as.factor(file$Block )
+anovaSQR<-aov(file$SQR ~ file$Cultivar*file$depth+file$Block)
+summary(anovaSQR)
+
+TukeyHSD(anovaSQR)
+(LSD.test(anovaSQR, "file$depth", alpha= 0.05, p.adj="none"))             
+summary (anovaSQR)
+
+
+
 #Analysing variable Red Colour intensity 
 #barplot 
 #The palette with grey:
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#D55E00", "#0072B2","#999999", "#E69F00", "#56B4E9", "#009E73", "#D55E00", "#0072B2","#56B4E9", "#009E73")
+
 #Here Analysis of cultivar 
+
+# For S
 file %>%
   filter(depth == "S") %>%
   group_by(Cultivar) %>%
@@ -102,9 +156,30 @@ file %>%
                 width=0.25,position = dodge_x)   +
   scale_fill_manual(values=cbPalette) + 
   theme_grey(base_size = 22) +
-  #facet_grid(depth~.) +
+  #facet_grid(depth ~.) +
   theme(axis.text.x=element_text(angle = +45,hjust = 1 ))+
-  ylab("Seed hardness")
+  ylab("% Seed hardness")
+
+
+# For B
+# For S
+file %>%
+  filter(depth == "B") %>%
+  group_by(Cultivar) %>%
+  summarise_each(funs(mean, sd)) %>%
+  mutate(Cultivar = factor(Cultivar,levels = 
+                             Cultivar[order(percentageHard_mean)])) %>%
+  ggplot(aes(x=Cultivar, y=percentageHard_mean, fill=Cultivar)) +
+  geom_bar(position = dodge_x, stat="identity") +
+  geom_errorbar(aes(ymin=percentageHard_mean-percentageHard_sd/2,
+                    ymax=percentageHard_mean+percentageHard_sd/2),
+                width=0.25,position = dodge_x)   +
+  scale_fill_manual(values=cbPalette) + 
+  theme_grey(base_size = 22) +
+  #facet_grid(depth ~.) +
+  theme(axis.text.x=element_text(angle = +45,hjust = 1 ))+
+  ylab("% Seed hardness")
+
 head(file)
 
 
